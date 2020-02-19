90ab51b35301eb45db5f1479c858ba45b849d080
IBM5100-H <190560895@qq.com>
2020-02-19 15:03:23 +0800

    第一次提交

---
+++ 作业/作业1/作业1/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movies;
+
+namespace 作业1.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            itcastEntities itcast = new itcastEntities();
+            var list = itcast.User_info;
+            return View(list);
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ 作业/作业1/作业1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="作业1.MvcApplication" Language="C#" %>
