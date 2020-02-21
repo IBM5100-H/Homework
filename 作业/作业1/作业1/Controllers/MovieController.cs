7756994ad3c94fe50f60a8bcfdb5419f091eeef4
IBM5100-H <190560895@qq.com>
2020-02-21 09:49:55 +0800

    一周

--- 作业/作业1/作业1/Controllers/MovieController.cs
+++ 作业/作业1/作业1/Controllers/MovieController.cs
@@ -5,30 +5,75 @@ using System.Web;
 using System.Web.Mvc;
 using Movies;
 
+
+
 namespace 作业1.Controllers
 {
     public class MovieController : Controller
     {
         // GET: Movie
+        itcastEntities itcast = new itcastEntities();
         public ActionResult Index()
         {
-            itcastEntities itcast = new itcastEntities();
             var list = itcast.User_info;
             return View(list);
         }
 
-        public ActionResult Delete()
+        public ActionResult Delete(string Id)
+        {
+            var user = itcast.User_info.Find(Convert.ToInt32(Id));
+            if (user != null)
+            {
+                itcast.User_info.Remove(user);
+                itcast.SaveChanges();
+                return Content("<script>alert('删除成功');window.location.href='/Movie/Index'; </script>");
+            }
+            else
+            {
+                return Content("<script>alert('删除失败');window.location.href='/Movie/Index'; </script>");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string Id)
+        {
+            var user = itcast.User_info.Find(Convert.ToInt32(Id));
+            if (user != null)
+            {
+                return View(user);
+            }
+            return View("Index");
+        }
+        [HttpPost]
+        public ActionResult Edit(User_info user_Info)
+        {
+            var user = itcast.User_info.Find(user_Info.Id);
+            if (user != null)
+            {
+                user.Name = user_Info.Name;
+                user.Age = user_Info.Age;
+                user.Company = user_Info.Company;
+                UpdateModel(user);
+                itcast.SaveChanges();
+                return Content("<script>alert('修改成功');window.location.href='/Movie/Index'; </script>");
+            }
+            return Content("<script>alert('修改失败');window.location.href='/Movie/Index'; </script>");
+        }
+
+
+        [HttpGet]
+        public ActionResult Insert()
         {
             return View();
         }
 
-        public ActionResult Select1()
+        [HttpPost]
+        public ActionResult Insert(User_info user_Info)
         {
-            return View();
+            itcast.User_info.Add(user_Info);
+            itcast.SaveChanges();
+            return Content("<script>alert('添加成功');window.location.href='/Movie/Index'; </script>");
         }
-        public ActionResult Select2()
-        {
-            return View();
-        }
+
     }
 }
