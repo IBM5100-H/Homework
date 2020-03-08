e42cf1246a8a480c6e93b0940226ea1bc6f889ca
IBM5100-H <190560895@qq.com>
2020-03-08 22:27:16 +0800

    layUI

--- 作业/作业1/作业1/Controllers/LayerUIController.cs
+++ 作业/作业1/作业1/Controllers/LayerUIController.cs
@@ -11,20 +11,39 @@ namespace 作业1.Controllers
     public class LayerUIController : Controller
     {
         // GET: LayerUI
-        itcastEntities itcast = new itcastEntities();
+        MovieEntities MovieEntities = new MovieEntities();
+
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Read()
+        public ActionResult Read(int page, int limit)
         {
-            var list = itcast.VMovie;
+            var list = MovieEntities.VMovieses;
             Hashtable ht = new Hashtable();
             ht["code"] = 0;
             ht["msg"] = "";
             ht["count"] = list.Count();
-            ht["data"] = list;
+            ht["data"] = list.SqlQuery("select top ("+ limit + ") * from(select row_number()over(order by  mid) as rownumber, *from [VMovieses]) temp_row where rownumber > ((" + page+ " - 1) * " + limit + ")").ToList();
+
             return Json(ht, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult del(string id)
+        {
+            Hashtable ht = new Hashtable();
+            var movie = MovieEntities.Movie.Find(Convert.ToInt32(id));
+            if (movie != null)
+            {
+                MovieEntities.Movie.Remove(movie);
+                MovieEntities.SaveChanges();
+                ht["message"] = "删除成功";
+                ht["result"] = "ture";
+            }
+            else
+            {
+                ht["message"] = "删除失败";
+            }
+            return Json(ht);
         }
     }
 }
